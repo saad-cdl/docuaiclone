1dfb2fdd42cc7381fb32b4e3a0e2b74782008321
Saad Ahmad Khan <cto@codexterlabs.com>
2025-06-01 08:06:56 -0400

    render changes

--- main.py
+++ main.py
@@ -306,4 +306,5 @@ def register():
     return render_template('register.html')
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)  # Run the Flask app on port 5000 with debug mode enabled
+    port = int(os.environ.get("PORT", 5000))  # Use the port Render assigns
+    app.run(host="0.0.0.0", port=port)  # Run the Flask app on port 5000 with debug mode enabled
